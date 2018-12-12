7d508fee7dac3dbf012ab25304fb40693b9bbfd6
Andrew <andrey_g.pt@mail.ru>
2018-12-12 07:44:44 +0200

    Worker, methods.

--- vuebnb-prototype/app.js
+++ vuebnb-prototype/app.js
@@ -1,33 +1,66 @@
 var app = new Vue({
-		el: '#app',
-		data: {
-			title: sample.title,
-			address: sample.address,
-			about: sample.about,
-			headerImageStyle: {
-				'background-image': 'url(sample/header.jpg)'
-			},
-			amenities: sample.amenities,
-			prices: sample.prices,
-			contracted: true,
-			modalOpen: false,
-			message: 'Hello world'
+	el: '#app',
+	data: {
+		title: sample.title,
+		address: sample.address,
+		about: sample.about,
+		headerImageStyle: {
+			'background-image': 'url(sample/header.jpg)'
 		},
-		watch: {
-			modalOpen: function() {
-				varclassName = 'modal-open';
-				if (this.modalOpen) {
-					document.body.classList.add(className);
-				} 
-				else {
-					document.body.classList.remove(className);
-				}
-			},
-
-			message: function(newVal, oldVal) {
-				console.log(oldVal, ', ', newVal); 
-			}
+		amenities: sample.amenities,
+		prices: sample.prices,
+		contracted: true,
+		modalOpen: false,
+		message: 'Hello world'
+	},
+	methods: {
+		myMethod: function(){
+			return this.message + ' World';
 		}
-	});
+	},
+	watch: {
+		modalOpen: function() {
+			var className = 'modal-open';
+			if (this.modalOpen) {
+				document.body.classList.add(className);
+			} 
+			else {
+				document.body.classList.remove(className);
+			}
+		},
 
-setTimeout(function() { app.message = 'Message';}, 2000);
+		message: function(newVal, oldVal) {
+			console.log(oldVal, ', ', newVal); 
+			alert(oldVal + '  ' + newVal);
+		}
+	},
+	beforeCreate: function (){},
+	created: function (){
+		document.addEventListener('keyup',escapeKeyListener);
+	},
+	beforeMount: function (){},
+	mounted: function (){},
+	beforeUpdate: function (){},
+	updated: function (){},
+	beforeDestroy: function (){
+		alert("beforeDestroy section");
+	},
+});
+
+function escapeKeyListener(evt) {
+	if (evt.keyCode === 27 &&app.modalOpen) {
+		app.modalOpen = false;
+	}
+}
+
+alert(app.myMethod());
+
+
+/*
+number=0;
+setInterval(function() { 
+	
+	app.message = 'Message ' + number;
+	number++;
+}, 1500);
+*/
