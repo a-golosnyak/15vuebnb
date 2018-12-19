7ae2088e72aa480fa89b30699143b3be7e3e9a9c
a-golosnyak <andrey_g.pt@mail.ru>
2018-12-19 17:43:00 +0200

    FullStackVueAndLaravel page 138. Work with Chapter05.

--- Chapter05/webpack.mix.js
+++ Chapter05/webpack.mix.js
@@ -1,5 +1,8 @@
 require('dotenv').config();
 let mix = require('laravel-mix');
+
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+
 mix
   .js('resources/assets/js/app.js', 'public/js')
   .styles([
@@ -15,3 +18,30 @@ mix
     open: false
   })
 ;
+
+mix.webpackConfig({
+   plugins: [
+      new BrowserSyncPlugin(
+         {
+            host: '192.168.10.10',
+            port: 3000,
+            proxy: 'http://app.app',
+            files: [
+               './**/*.css',
+               './app/**/*',
+               './config/**/*',
+               './resources/views/**/*',
+               './routes/**/*'
+            ],
+            watchOptions: {
+              usePolling: true,
+              interval: 500
+            },
+            open: false
+         },
+         {
+            reload: false
+         }
+      )
+   ]
+})
